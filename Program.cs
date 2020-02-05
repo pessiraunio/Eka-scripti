1a1ff38063d12a61233746ac42e6432b855fc7b9
pessiraunio <60428514+pessiraunio@users.noreply.github.com>
2020-02-05 12:55:25 +0200

    Add files via upload
    jeeee

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Projekti
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {   //Kysytään käyttäjältä käyttäjätunnus
+            Console.Write("Käyttäjätunnus: ");
+            String syöte=Console.ReadLine();
+
+            if (syöte == "pepo") //Tarkistetaan tunnuksen oikeellisuus
+            {
+                Console.Write("Salasana: ");
+                String salasana = Console.ReadLine();
+                if (salasana == "perunamaa")
+                {
+                    Console.WriteLine("Kirjautuminen onnistui");
+                }
+
+                else
+                {
+                    Console.WriteLine("Väärä salasana");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Väärä käyttäjätunnus");
+            }
+
+
+            
+        
+        
+        }
+
+
+         
+            
+        }
+    }
